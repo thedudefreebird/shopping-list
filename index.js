a1b74c2a0fe8fd462eb3b61f3f18ac60a5f7f43d
thedudefreebird <tyler.freebery@gmail.com>
2019-10-14 17:03:30 -0400

    added code for Add item

---
+++ index.js
@@ -0,0 +1,14 @@
+function handledClickedItems(){
+	$('.js-shopping-list-form').submit(event => { 
+		event.preventDefault();
+		const userTextElement = $(event.currentTarget).find('.shopping-list-entry');
+		$("#shopping-list").append("li" + userTextElement);
+	
+	});
+
+	$('ul').on('li', function(event){
+		this.add();
+	});
+}
+
+$(handledClickedItems);
