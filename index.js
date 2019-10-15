18b876576f8c7f08f4b4f0475966bf2fd7ccc56f
thedudefreebird <tyler.freebery@gmail.com>
2019-10-15 17:50:17 -0400

    fixed append for add item

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function handledClickedItems(){
-	$('#js-shopping-list-form').submit(event => { 
+	$('#js-shopping-list-form').submit(event => {
 		event.preventDefault();
 		const userTextElement = $(event.currentTarget).find('#shopping-list-entry');
-		$('ul').append('<li>' + userTextElement + '</li>');
-	
+		$('.shopping-list').append(
+		`<li>
+        <span class="shopping-item"> + userTextElement + </span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      	</li>`);
+
 	});
 
-	$('ul').on('click', function(event){
-		this.add();
-	});
+	//$('ul').on('click', function(event){
+		//this.add();
+	//});
 }
 
 $(handledClickedItems);
