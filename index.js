a0ffa7c66edce2145acb775639d8e4a16996ed5e
thedudefreebird <tyler.freebery@gmail.com>
2019-10-15 18:35:22 -0400

    cleaned up, working add item button

--- index.js
+++ index.js
@@ -15,27 +15,6 @@ function addedItem(){
 		</li>
 			`);
 	});
-
-
-	/*let addItem = $('button').submit();
-	addItem.on('click', function(event){
-		event.preventDefault();
-		let userTextElement = $(event.currentTarget).find('shopping-list-entry').value;
-		$('.shopping-list').append(`
-			<li>
-        <span class="shopping-item"> `+ userTextElement +` </span>
-        <div class="shopping-item-controls">
-          <button class="shopping-item-toggle">
-            <span class="button-label">check</span>
-          </button>
-          <button class="shopping-item-delete">
-            <span class="button-label">delete</span>
-          </button>
-        </div>
-      </li>
-			`);
-	});*/
-
 }
 
 
